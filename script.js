1e42182b2790b63f8853c002942b99030ceeb081
zisanahmed97 <zisanahmed97@gmail.com>
2026-03-09 14:30:48 +0600

    added displayIssues function

--- script.js
+++ script.js
@@ -13,11 +13,79 @@ allIssues = data.data
 
 document.getElementById("issueCount").innerText = allIssues.length
 
-console.log(allIssues);
+displayIssues(allIssues);
 
 }
 
+function displayIssues(issues){
+
+const container = document.getElementById("issuesContainer")
+
+container.innerHTML = ""
+
+issues.forEach(issue => {
+
+const card = document.createElement("div")
+
+let borderColor =
+issue.status === "open"
+? "border-green-500"
+: "border-purple-500"
+
+card.className =
+`bg-white border-t-4 ${borderColor} rounded-lg shadow p-4 cursor-pointer`
+
+card.innerHTML = `
+
+<div class="flex justify-between items-center mb-2">
+
+<span class="text-green-500 text-xl">✔</span>
+
+<span class="text-xs px-3 py-1 rounded-full
+${issue.priority === "high"
+? "bg-red-100 text-red-500"
+: issue.priority === "medium"
+? "bg-yellow-100 text-yellow-600"
+: "bg-gray-200 text-gray-500"}">
+
+${issue.priority.toUpperCase()}
+
+</span>
+
+</div>
+
+<h2 class="font-semibold text-gray-800">
+${issue.title}
+</h2>
+
+<p class="text-sm text-gray-500 mt-2">
+${issue.description}
+</p>
+
+<div class="flex gap-2 mt-3">
+
+<span class="text-xs px-2 py-1 bg-red-100 text-red-500 rounded-full">
+BUG
+</span>
+
+<span class="text-xs px-2 py-1 bg-yellow-100 text-yellow-600 rounded-full">
+HELP WANTED
+</span>
+
+</div>
+
+<div class="border-t mt-4 pt-2 text-xs text-gray-500">
+
+<p>#${issue.id} by ${issue.author}</p>
+
+<p>${issue.createdAt}</p>
+
+</div>
+
+`
 
 
 
-loadIssues();
+
+
+loadIssues()
